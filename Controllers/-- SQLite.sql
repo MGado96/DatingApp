2f291b8432c1d047d936ee16097ba8e25381a6c6
Mohannad Gado <mohannadgado@gmail.com>
2022-09-14 19:47:31 +0200

    End of section 2

---
+++ Controllers/-- SQLite.sql
@@ -0,0 +1,12 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Gado");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Nido");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Misho");
+
+INSERT INTO Users (Id, UserName)
+VALUES (4, "Bizo");
